76512cb01456eeff75fa8e1010ecbcaee9e451b3
Diego Patiño <dapny125@gmail.com>
2016-11-03 12:48:11 -0400

    1st Version

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+var friendList = [
+  {
+    name: 'Diego Patino',
+    profilePic: 'https://avatars1.githubusercontent.com/u/19331006?v=3&u=f0fe1ba7468215fdd194ff6d1db724d7f904439f&s=140',
+    scores: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+
+]
+
+module.exports = friendList;
